aaece2f72dd35aecab5a10e7094410b80b639418
Antoine <antoinebouclier1@gmail.com>
2026-02-09 09:24:39 +0100

    Container: ex02 Done.

--- Container/ex02.cpp
+++ Container/ex02.cpp
@@ -2,35 +2,39 @@
 #include <vector>
 #include <list>
 
-/* void test_vector(void)
-{
-	std::vector<int>			v = {1, 2, 3, 4, 5};
-	std::vector<int>::iterator	it;
-
-	it = v.begin();
-	v.insert(it + 2, 42);
-	for (it = v.begin(); it != v.end(); ++it)
-		std::cout << *it << std::endl;
-} */
-
 void test_vector(void)
 {
-	std::vector<int>			v = {1, 2, 3, 4, 5};
+	std::vector<int>			v;
 	std::vector<int>::iterator	it;
 
+	for (int i = 1; i <= 5; i++)
+		v.push_back(i);
 	it = v.begin();
 	v.insert(it + 2, 42);
 	for (it = v.begin(); it != v.end(); ++it)
-		std::cout << *it << std::endl;
+		std::cout << *it << ' ';
+	std::cout << std::endl;
 }
 
 void test_list(void)
 {
-	std::list<int>	l = {1, 2, 3, 4, 5};
+	std::list<int>				l;
+	std::list<int>::iterator	it;
+
+	for (int i = 1; i <= 5; i++)
+		l.push_back(i);
+	it = l.begin();
+	++it;
+	++it;
+	l.insert(it, 42);
+	for (it = l.begin(); it != l.end(); ++it)
+		std::cout << *it << ' ';
+	std::cout << std::endl;
 }
 
 int	main(void)
 {
 	test_vector();
+	test_list();
 	return 0;
 }
