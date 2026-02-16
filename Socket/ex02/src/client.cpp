db85b6e5dfdf63f36d270d8c6ff329904ef56264
Antoine <antoinebouclier1@gmail.com>
2026-02-16 08:54:26 +0100

    Socket: Ex02: update

--- Socket/ex02/src/client.cpp
+++ Socket/ex02/src/client.cpp
@@ -5,6 +5,22 @@
 
 #include <iostream>
 
+bool send_all(int socket, const char* buffer, size_t length)
+{
+	size_t totalSent = 0;
+	
+	while (totalSent < length)
+	{
+		ssize_t sent;
+
+		sent = send(socket, buffer + totalSent, length - totalSent, 0);
+		if (sent <= 0)
+			return (false);
+		totalSent += sent;
+	}
+	return (true);
+}
+
 int	main(void)
 {
 	/* Creating the client socket */
--- Socket/ex02/src/server.cpp
+++ Socket/ex02/src/server.cpp
@@ -2,6 +2,8 @@
 #include <netinet/in.h> // sockadrr_in
 #include <unistd.h> // close()
 #include <string.h>
+#include <vector>
+#include <poll.h>
 
 #include <iostream>
 
@@ -36,4 +38,72 @@ int	main(void)
 	close(serverSocket);
 
 	return (0);
+}
+
+int main(void)
+{
+	/* Creating the server socket */
+	int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+	// AF_INET -> Internet protocol v4 addresses (IPV4)
+	// SOCK_STREAM -> TCP socket /* Defining server adress */
+
+	sockaddr_in serverAddress;
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8080);
+	// Converts port to network byte order. serverAddress.sin_addr.s_addr = INADDR_ANY;
+	// Accept connections on any IP.
+	
+	/* Bind socket to adress */
+	bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+	
+	/* Listen for incomming connections */
+	listen(serverSocket, 5);
+	
+	std::vector<pollfd> fds;
+	pollfd serverPoll;
+	
+	serverPoll.fd = serverSocket;
+	serverPoll.events = POLLIN;
+	fds.push_back(serverPoll);
+	
+	while (1)
+	{
+		poll(fds.data(), fds.size(), -1);
+		for (size_t i = 0; i < fds.size(); i++)
+		{
+			if (fds[i].revents & POLLIN)
+			{
+				if (fds[i].fd == serverSocket)
+				{
+					/* Accept client connection */
+					int clientSocket = accept(serverSocket, NULL, NULL);
+					
+					/* Adding new pollfd */
+					pollfd clientPoll;
+					
+					clientPoll.fd = clientSocket;
+					clientPoll.events = POLLIN;
+					fds.push_back(clientPoll);
+				}
+				else
+				{
+					/* Handle message */
+					char buffer[1024];
+
+					memset(buffer, 0, sizeof(buffer));
+					int bytes = recv(fds[i].fd, buffer, sizeof(buffer), 0);
+					if (bytes <= 0)
+					{
+						close(fds[i].fd);
+						fds.erase(fds.begin() + i);
+						i--;
+					}
+					else
+						std::cout << "Message: " << buffer << std::endl;
+				}
+			}
+		}
+	}
+	/* close server socket */
+	close(serverSocket); return (0);
 }
